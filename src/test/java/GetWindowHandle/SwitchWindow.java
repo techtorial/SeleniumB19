4b55a910fc65cc8bb81bb8df22c71f7d2b727a69
techtorial <50930535+techtorial@users.noreply.github.com>
2024-02-27 19:11:58 -0600

    Latest

--- src/test/java/GetWindowHandle/SwitchWindow.java
+++ src/test/java/GetWindowHandle/SwitchWindow.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.BrowserUtils;
 
@@ -35,4 +36,47 @@ public class SwitchWindow {
         header=driver.findElement(By.tagName("h3"));
         System.out.println(BrowserUtils.getText(header));
     }
+
+    @Test
+    public void switchWindowPractice() throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+        WebElement button2=driver.findElement(By.cssSelector("#newTabBtn"));
+        Thread.sleep(3000);
+        BrowserUtils.scrollIntoView(driver,button2);
+        button2.click();
+        String mainPageId=driver.getWindowHandle();//mainPageId
+        Set<String> allPagesID=driver.getWindowHandles();//allPagesID
+
+        for(String id:allPagesID){
+            if(!id.equals(mainPageId)){
+                driver.switchTo().window(id);
+            }
+        }
+        WebElement header=driver.findElement(By.xpath("//h1[@itemprop='name']"));
+        String actualHeader=BrowserUtils.getText(header);
+        String expectedHeader="AlertsDemo";
+        Assert.assertEquals(actualHeader,expectedHeader);
+//
+        /*
+    1-Navigate to the website
+    2-Click Button2 if it throws exception then scroll to the button
+    3-Validate the header "AlertsDemo"
+    4-Click the first click me button
+       */
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
