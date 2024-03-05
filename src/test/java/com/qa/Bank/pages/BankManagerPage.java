b13ccf9b04dd0833caa2d3d2ea125e69a7415102
techtorial <50930535+techtorial@users.noreply.github.com>
2024-03-04 20:32:29 -0600

    Latest

--- src/test/java/com/qa/Bank/pages/BankManagerPage.java
+++ src/test/java/com/qa/Bank/pages/BankManagerPage.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import utils.BrowserUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BankManagerPage {
 
@@ -23,6 +28,14 @@ public class BankManagerPage {
     WebElement zipCode;
     @FindBy(xpath = "//button[@type='submit']")
     WebElement submitCustomer;
+    @FindBy(xpath = "//button[@ng-class='btnClass2']")
+    WebElement openAccountButton;
+    @FindBy(css = "#userSelect")
+    WebElement customerName;
+    @FindBy(css = "#currency")
+    WebElement currency;
+    @FindBy(xpath = "//button[.='Process']")
+    WebElement processButton;
 
     public void addCustomerInformation(WebDriver driver,String firstName,String lastName,String zipCode,
     String expectedMessage){
@@ -35,6 +48,25 @@ public class BankManagerPage {
         Assert.assertTrue(alert.getText().contains(expectedMessage));
         alert.accept();
     }
+
+    public void openAccountInformation(WebDriver driver,String customerName,String option1,String option2,String option3,String option4,
+                                       String expectedMessage){
+        openAccountButton.click();
+        BrowserUtils.selectBy(this.customerName,customerName,"text");//Ahmet Baldir
+        Select currencyBox=new Select(currency);
+        List<WebElement> actualCurrencies=currencyBox.getOptions();//4 options
+        List<String> expectedCurrencies= Arrays.asList(option1,option2,option3,option4);
+
+        for(int i=0;i<actualCurrencies.size();i++){ //you validates all options from currency
+            Assert.assertEquals(BrowserUtils.getText(actualCurrencies.get(i)),expectedCurrencies.get(i));
+        }
+        BrowserUtils.selectBy(currency,option2,"value");//you choose Dollar
+        processButton.click();
+        Alert alert=driver.switchTo().alert();
+        Assert.assertTrue(alert.getText().contains(expectedMessage));
+        alert.accept();
+    }
+
 
 
 
--- src/test/java/com/qa/Bank/tests/BankManagerTest.java
+++ src/test/java/com/qa/Bank/tests/BankManagerTest.java
@@ -22,6 +22,20 @@ public class BankManagerTest {
         bankManagerPage.addCustomerInformation(driver,"Ahmet","Baldir","13123",
                 "Customer added successfully");
     }
+    @Test
+    public void validateOpenAccountFunctionality(){
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        BankLoginPage bankLoginPage=new BankLoginPage(driver);
+        bankLoginPage.clickManagerLoginButton();
+        BankManagerPage bankManagerPage=new BankManagerPage(driver);
+        bankManagerPage.addCustomerInformation(driver,"Ahmet","Baldir","13123",
+                "Customer added successfully");
+        bankManagerPage.openAccountInformation(driver,"Ahmet Baldir","---Currency---",
+                "Dollar","Pound","Rupee","Account created successfully");
+    }
 
 
 }
